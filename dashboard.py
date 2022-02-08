3b290700603f7cdfa12a928894bcae27fbd31464
Yo <yoyo5022@gmail.com>
2022-02-08 10:56:34 +0800

    Route: hiStaff_dashapp succeed!

--- dashboard.py
+++ dashboard.py
@@ -1,25 +1,41 @@
-from dash import Dash, dcc, html
+from distutils.log import debug
+from dash import Dash, dcc, html, callback, Input, Output
 
 
 def init_dashboard(server):
     """Create a Plotly Dash dashboard."""
     dash_app = Dash(
         server=server,
-        routes_pathname_prefix='/dashapp/',
-        #external_stylesheets=['/static/dist/css/styles.css',]
+        #requests_pathname_prefix='/hiStaff_dashapp/',
+        routes_pathname_prefix='/hiStaff_dashapp/',
+        external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'],
     )
 
     # Create Dash Layout
-    dash_app.layout = html.Div(id='dash-container')
+    dash_app.layout = html.Div(children=[
+        # represents the browser address bar and doesn't render anything
+        dcc.Location(id='url', refresh=False),
+
+        dcc.Link('Navigate to "CHECK IN TABLE"', href=f'/checkin'),
+        html.Br(),
+        dcc.Link('Navigate to "CHECK OUT TABLE"', href='/checkout'),
+
+        # content will be rendered in this element
+        html.Div(id='page-content', children=[])
+    ])
 
     # Initialize callbacks after our app is loaded
-    init_callbacks(dash_app)    
+    init_callbacks()    
 
     return dash_app.server
 
 
-def init_callbacks(dash_app):
-    @dash_app.callback()
-    def update_graph(rows):
-        # Callback logic
-        pass
+def init_callbacks():
+    @callback(
+        Output(component_id='page-content', component_property='children'), 
+        [Input(component_id='url', component_property='pathname')]
+        )
+    def display_page(pathname):
+        return html.Div([
+            html.H3(f'You are on page {pathname}')
+        ])
